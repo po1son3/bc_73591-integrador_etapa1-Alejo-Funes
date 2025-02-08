d7b3fe0deb14b162a454c2c6e6277cf5d317901f
Alejo Funes <alefunesrios@gmail.com>
2025-02-08 17:10:11 -0300

    agrego-paletas-colores

---
+++ src/db/productos.js
@@ -0,0 +1,39 @@
+const productos = [
+    { 
+      id: 1, 
+      nombre: "Iphone 11 Pro Max", 
+      foto: "imgs/apple-iphone-11-pro-max.jpg", 
+      descripcion: "Iphone 11 super pro max", 
+      precio: 222.20
+    },
+    { 
+      id: 2, 
+      nombre: "IPhone 12", 
+      foto: "imgs/apple-iphone-12.jpg", 
+      descripcion: "Iphone 12, flojito", 
+      precio: 333.30
+    },
+    { 
+      id: 3, 
+      nombre: "Camara Canon", 
+      foto: "imgs/camara-canon.jpg", 
+      descripcion: "Camara ultra 4k", 
+      precio: 444.40
+    },
+    { 
+      id: 4, 
+      nombre: "Drone DJI", 
+      foto: "imgs/drone-dji.jpg", 
+      descripcion: "Alto drone", 
+      precio: 555.50
+    },
+    { 
+      id: 5, 
+      nombre: "Auriculares Sony", 
+      foto: "imgs/auriculares-sony.jpg", 
+      descripcion: "Cancelación activa!", 
+      precio: 666.60
+    }
+]
+
+export default productos
--- src/main.js
+++ src/main.js
@@ -1,1 +1,175 @@
+import productos from './db/productos'
 import './sass/main.scss'
+
+console.log(productos) // array Productos
+
+const contenedorProductos = document.getElementById('container-productos')
+console.log(contenedorProductos)
+
+
+const start = () => {
+    console.warn('Se cargó todo el HTML')
+
+    let html = ''
+    //debugger
+    productos.forEach(prod => {
+        console.log(prod)
+        //debugger
+        // html = html + '' === html += ''
+        html += `<div class="card">
+            <article class="card__article">
+                <div class="card__image-container">
+                    <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+                </div>
+                <div class="card__content">
+                    <h2 class="card__heading">${prod.nombre}</h2>
+                    <div class="card__description">
+                        <p>${prod.descripcion}</p>
+                    </div>
+                </div>
+            </article>
+        </div>`
+    })
+    // debugger
+    console.log(html) /* Voy a tener varios div.card * 5 -> */
+
+    contenedorProductos.innerHTML = html
+
+}
+
+/*   html += `<div class="card">
+            <article class="card__article">
+                <div class="card__image-container">
+                    <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+                </div>
+                <div class="card__content">
+                    <h2 class="card__heading">${prod.nombre}</h2>
+                    <div class="card__description">
+                        <p>${prod.descripcion}</p>
+                    </div>
+                </div>
+            </article>
+        </div>` */
+
+const start2 = () => {
+
+    productos.forEach(prod => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        //console.log(card)
+        const articulo = document.createElement('article')
+        articulo.classList.add('card__article')
+        //console.log(articulo)
+        card.appendChild(articulo)
+        //console.log(card)
+        const cardImageContainer = document.createElement('div')
+        cardImageContainer.classList.add('card__image-container')
+        const foto = document.createElement('img')
+        foto.classList.add('card__image')
+        foto.src = prod.foto
+        foto.alt = prod.nombre
+        cardImageContainer.appendChild(foto)
+        // console.log(cardImageContainer)
+        articulo.appendChild(cardImageContainer)
+        //console.log(card)
+
+        /* <div class="card__content">
+                    <h2 class="card__heading">${prod.nombre}</h2>
+                    <div class="card__description">
+                        <p>${prod.descripcion}</p>
+                    </div>
+        </div> */
+
+        const cardContent = document.createElement('div')
+        const h2Titulo = document.createElement('h2')
+        h2Titulo.classList.add('card__heading')
+        h2Titulo.textContent = prod.nombre
+        //console.log(h2Titulo)
+        const cardDescripcion = document.createElement('div')
+        cardDescripcion.classList.add('card__description')
+        const parrafo = document.createElement('p')
+        parrafo.textContent = prod.descripcion
+        //console.log(parrafo)
+
+        cardContent.appendChild(h2Titulo)
+        cardDescripcion.appendChild(parrafo)
+        cardContent.appendChild(cardDescripcion)
+        //console.log(cardContent)
+        articulo.appendChild(cardContent)
+        console.log(card)
+
+        contenedorProductos.appendChild(card)
+
+
+    })
+
+
+
+    
+}
+
+const start3 = () => {
+
+    const fragmento = new DocumentFragment();
+
+    productos.forEach(prod => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        //console.log(card)
+        const articulo = document.createElement('article')
+        articulo.classList.add('card__article')
+        //console.log(articulo)
+        card.appendChild(articulo)
+        //console.log(card)
+        const cardImageContainer = document.createElement('div')
+        cardImageContainer.classList.add('card__image-container')
+        const foto = document.createElement('img')
+        foto.classList.add('card__image')
+        foto.src = prod.foto
+        foto.alt = prod.nombre
+        cardImageContainer.appendChild(foto)
+        // console.log(cardImageContainer)
+        articulo.appendChild(cardImageContainer)
+        //console.log(card)
+
+        /* <div class="card__content">
+                    <h2 class="card__heading">${prod.nombre}</h2>
+                    <div class="card__description">
+                        <p>${prod.descripcion}</p>
+                    </div>
+        </div> */
+
+        const cardContent = document.createElement('div')
+        const h2Titulo = document.createElement('h2')
+        h2Titulo.classList.add('card__heading')
+        h2Titulo.textContent = prod.nombre
+        //console.log(h2Titulo)
+        const cardDescripcion = document.createElement('div')
+        cardDescripcion.classList.add('card__description')
+        const parrafo = document.createElement('p')
+        parrafo.textContent = prod.descripcion
+        //console.log(parrafo)
+
+        cardContent.appendChild(h2Titulo)
+        cardDescripcion.appendChild(parrafo)
+        cardContent.appendChild(cardDescripcion)
+        //console.log(cardContent)
+        articulo.appendChild(cardContent)
+        console.log(card)
+
+        fragmento.appendChild(card)
+        
+
+    })
+
+    console.log(fragmento) /* Los 5 div cards */
+
+    // Intervengo una sola vez el DOM.
+    contenedorProductos.appendChild(fragmento)
+
+    
+}
+
+//                          evento        | callback
+window.addEventListener('DOMContentLoaded', start3) /* se dispara cuando todo el html fue leido (cargado) */
+
